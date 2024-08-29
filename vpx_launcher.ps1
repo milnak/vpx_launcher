4ca2c17de2deacedfecc7410a7df7cea81a7257a
Jeff <33181256+milnak@users.noreply.github.com>
2024-08-29 09:41:11 -0700

    Lots of enhancements.

--- vpx_launcher.ps1
+++ vpx_launcher.ps1
@@ -1,14 +1,31 @@
-# BUGBUG: Bobby Orr doesn't show  up until sorted.
+[CmdletBinding()]
 Param(
+    [string]$PinballExe = 'D:\Visual Pinball\VPinballX64.exe',
     [string]$TablePath = 'D:\Visual Pinball\Tables',
     [string]$RomPath = 'D:\Visual Pinball\VPinMAME\roms',
-    [string]$ExePath = 'D:\Visual Pinball\VPinballX64.exe'
+    [string]$Database = '.\vpx_launcher.csv'
 )
+
+function Invoke-Game {
+    Param(
+        [Parameter(Mandatory)][Windows.Forms.Button]$LaunchButton,
+        [Parameter(Mandatory)][string]$PinballExe,
+        [Parameter(Mandatory)][string]$TablePath
+    )
+
+    $prevText = $buttonLaunch.Text
+    $buttonLaunch.Enabled = $false
+    $buttonLaunch.Text = 'Running'
+
+    Start-Process -FilePath $PinballExe -ArgumentList '-ExtMinimized', '-Play', ('"{0}"' -f $TablePath) -NoNewWindow -Wait
+
+    $buttonLaunch.Enabled = $true
+    $buttonLaunch.Text = $prevText
+}
 
 function Invoke-Dialog {
     Param($Data)
 
-    $script:selectedItem = $null
     $script:listViewSort = @{
         Column     = 0
         Descending = $false
@@ -48,9 +65,7 @@ function Invoke-Dialog {
 
     $listView.add_SelectedIndexChanged({
             if ($listView.SelectedItems.Count -gt 0) {
-                $script:selectedItem = $listView.SelectedItems.Tag + '.vpx'
-
-                $label1.Text = $listView.SelectedItems.Text
+                $label1.Text = $listView.SelectedItems.Tag
             }
         })
 
@@ -78,18 +93,9 @@ function Invoke-Dialog {
     $listView.add_MouseDoubleClick(
         {
             # $_ : Windows.Forms.MouseEventArgs
-            $item = Join-Path $TablePath ($listView.SelectedItems.Tag + '.vpx')
+            $tablePath = Join-Path $TablePath $listView.SelectedItems.Tag
 
-            # TODO: Refactor passing $item
-            $prevText = $buttonLaunch.Text
-            $buttonLaunch.Enabled = $false
-            $buttonLaunch.Text = 'Running'
-
-            Start-Process -FilePath $ExePath -ArgumentList '-ExtMinimized', '-Play', ('"{0}"' -f $item) -NoNewWindow -Wait
-
-            $buttonLaunch.Enabled = $true
-            $buttonLaunch.Text = $prevText
-
+            Invoke-Game -LaunchButton $buttonLaunch -PinballExe $PinballExe -TablePath $tablePath
         }
     )
 
@@ -104,33 +110,29 @@ function Invoke-Dialog {
     $label1.Text = ''
     $label1.Font = New-Object  System.Drawing.Font('Segoe UI', 16, [Drawing.FontStyle]::Bold)
     $label1.Location = New-Object -TypeName 'Drawing.Point' -ArgumentList 0, 0
-    $label1.AutoSize = $true
+    $label1.Width = 440
+    $label1.Height = 30
+    $label1.AutoSize = $false
     $panelStatus.Controls.Add($label1)
 
     $label2 = New-Object -TypeName 'Windows.Forms.Label'
     $label2.Text = ('{0} Machines' -f $listView.Items.Count)
     $label2.Location = New-Object -TypeName 'Drawing.Point' -ArgumentList 3, 40
-    $label2.AutoSize = $true
+    $label1.Width = 440
+    $label2.AutoSize = $false
     $panelStatus.Controls.Add($label2)
 
     $buttonLaunch = New-Object -TypeName 'Windows.Forms.Button'
     $buttonLaunch.Location = New-Object -TypeName 'Drawing.Size' -ArgumentList 450, 0
-    $buttonLaunch.Size = New-Object -TypeName 'Drawing.Size' -ArgumentList 120, 40
+    $buttonLaunch.Size = New-Object -TypeName 'Drawing.Size' -ArgumentList 118, 40
     $buttonLaunch.Text = 'Launch'
     $panelStatus.Controls.Add($buttonLaunch)
 
     $buttonLaunch.Add_Click(
         {
-            $item = Join-Path $TablePath $script:selectedItem
+            $tablePath = Join-Path $TablePath $listView.SelectedItems.Tag
 
-            $prevText = $buttonLaunch.Text
-            $buttonLaunch.Enabled = $false
-            $buttonLaunch.Text = 'Running'
-
-            Start-Process -FilePath $ExePath -ArgumentList '-ExtMinimized', '-Play', ('"{0}"' -f $item) -NoNewWindow -Wait
-
-            $buttonLaunch.Enabled = $true
-            $buttonLaunch.Text = $prevText
+            Invoke-Game -LaunchButton $buttonLaunch -PinballExe $PinballExe -TablePath $tablePath
 
             # $form.DialogResult = [Windows.Forms.DialogResult]::OK
             # $form.Close() | Out-Null
@@ -151,53 +153,89 @@ function Invoke-Dialog {
     $form.ShowDialog()
 }
 
-$totalSize = 0
-$tableData = Get-Content -LiteralPath 'tables.csv' | ConvertFrom-Csv -Header 'Filename', 'Table', 'Manufacturer', 'Year', 'ROM'
-$tables = foreach ($item in (Get-ChildItem -File -LiteralPath $TablePath -Include '*.vpx')) {
-    $baseName = $item.BaseName
-    $found = $tableData | Where-Object Filename -eq $baseName
+function Read-Database {
+    param (
+        [string]$DatabasePath,
+        [string]$RomPath
+    )
 
-    $totalSize += $item.Length
-
-    if (!$found) {
-        Write-Warning ('Table not found in database: "{0}"' -f $baseName)
-        [PSCustomObject]@{
-            Filename     = [IO.Path]::GetFileNameWithoutExtension($item.FullName)
-            Table        = $baseName
-            Manufacturer = '?'
-            Year         = '?'
-        }
+    $totalSize = 0
+    $tableData = $null
+    if ($DatabasePath.Length -ne 0) {
+        $tableData = Get-Content -LiteralPath $DatabasePath | ConvertFrom-Csv -Header 'Filename', 'Table', 'Manufacturer', 'Year', 'ROM'
     }
-    else {
-        if ($found.ROM.Length -eq 0) {
-            # No ROM needed
-            if ([int]$found.Year -gt 1977) {
-                # Machines after 1977 likely require a ROM.
-                Write-Warning ('Database claims table "{0}" has no ROM?' -f $baseName)
-            }
 
-            $found
-        }
-        else {
-            $rom = $found.ROM + '.zip'
-            $romItem = Get-Item -ErrorAction SilentlyContinue -LiteralPath (Join-Path $RomPath $rom)
-            if (!$romItem) {
-                Write-Warning ('Table "{0}" ROM "{1}" not found' -f $baseName, $rom)
+    $data = foreach ($item in (Get-ChildItem -File -LiteralPath $TablePath -Include '*.vpx')) {
+        $baseName = $item.BaseName
+        $found = $tableData | Where-Object Filename -eq $baseName
+
+        $totalSize += $item.Length
+
+        if (!$found) {
+            Write-Warning ('Table not found in database: "{0}"' -f $baseName)
+
+            # Use regex to try to guess table, manufacturer and year from filename.
+            if ($baseName -match '(.+)[ _]\((.+) (\d{4})\)') {
+                [PSCustomObject]@{
+                    Filename     = $item.Name
+                    Table        = $matches[1]
+                    Manufacturer = $matches[2]
+                    Year         = $matches[3]
+                }
             }
             else {
-                # ROM found
-                $totalSize += $romItem.Size
+                [PSCustomObject]@{
+                    Filename     = $item.Name
+                    Table        = $baseName
+                    Manufacturer = '?'
+                    Year         = '?'
+                }
+            }
+        }
+        else {
+            if ($found.ROM.Length -eq 0) {
+                # No ROM needed
+                if ([int]$found.Year -gt 1977) {
+                    # Machines after 1977 likely require a ROM.
+                    Write-Warning ('Database claims table "{0}" has no ROM?' -f $baseName)
+                }
+
                 $found
+            }
+            elseif ($RomPath.Length -ne 0) {
+                # If $RomPath specified, check to see if the rom file exists.
+                $rom = $found.ROM + '.zip'
+                $romItem = Get-Item -ErrorAction SilentlyContinue -LiteralPath (Join-Path $RomPath $rom)
+                if (!$romItem) {
+                    Write-Warning ('Table "{0}" ROM "{1}" not found' -f $baseName, $rom)
+                }
+                else {
+                    # ROM found
+                    $totalSize += $romItem.Size
+                    $found
+                }
             }
         }
     }
+
+    # Return list sorted by table (friendly name).
+    $data.GetEnumerator() | Sort-Object -Property Table
+
+    Write-Verbose ('Table and ROM size: {0:N0} bytes' -f $totalSize)
 }
 
+# Verify paths. Database and RomPath are optional.
+
+Get-Item -ErrorAction Stop -LiteralPath $PinballExe | Out-Null
+Get-Item -ErrorAction Stop -LiteralPath $TablePath | Out-Null
+
+# Read in database
+
+$tables = Read-Database -DatabasePath $Database -RomPath $RomPath
 if ($tables.Count -eq 0) {
     Write-Warning "No tables found in $TablePath"
     return
 }
 
-Write-Host ('Table and ROM size: {0:N0} bytes' -f $totalSize)
 if ((Invoke-Dialog -Data $tables) -eq [Windows.Forms.DialogResult]::OK) {
 }
