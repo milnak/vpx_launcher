4b940d74372037213295e29b6ab1d347b83164d3
Jeff <33181256+milnak@users.noreply.github.com>
2024-09-06 23:09:50 -0700

    Simple tool to check for vpx or vpm updates.

---
+++ UpdateCheck.ps1
@@ -0,0 +1,60 @@
+﻿Param([string]$Path = '.')
+
+### Visual Pinball X
+
+Write-Host -ForegroundColor Cyan 'Visual Pinball X:'
+
+$vpx = Join-Path -Path $Path -ChildPath 'VPinballX64.exe'
+
+$vpxVer = 'v{0}' -f (Get-Item  $vpx -ErrorAction SilentlyContinue).VersionInfo.ProductVersion
+if (!$vpxVer) {
+    "Unable to find $vpx"
+    return
+}
+
+try {
+    $req = Invoke-WebRequest -Uri 'https://api.github.com/repos/vpinball/vpinball/releases'
+}
+catch {
+    Write-Warning "Unable to connect to api.github.com: $_"
+    return
+}
+
+$json = $req.Content | ConvertFrom-Json
+$tag = $json.tag_name[0] -replace '-','.'
+"Online version: $tag"
+"Local version:  $vpxVer"
+
+if ($vpxVer -ne $tag) {
+    Write-Host -ForegroundColor Yellow 'VPX Update available.'
+}
+
+### Visual PinMAME
+
+Write-Host -ForegroundColor Cyan 'Visual PinMAME:'
+
+$vpm = Join-Path -Path $Path -ChildPath 'VPinMAME\VPinMAME64.dll'
+
+$vpmVer = 'v{0}' -f (Get-Item  $vpm -ErrorAction SilentlyContinue).VersionInfo.ProductVersion
+if (!$vpmVer) {
+    "Unable to find $vpm"
+    return
+}
+
+try {
+    $req = Invoke-WebRequest -Uri 'https://api.github.com/repos/vpinball/pinmame/releases'
+} 
+catch {
+    Write-Warning "Unable to connect to api.github.com: $_"
+    return
+}
+
+$json = $req.Content | ConvertFrom-Json
+$tag = $json.tag_name[0] -replace '-','.'
+"Online version: $tag"
+"Local version:  $vpmVer"
+
+if ($vpmVer -ne $tag) {
+    Write-Host -ForegroundColor Yellow 'VPM Update available.'
+}
+
