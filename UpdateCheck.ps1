f7c7542c2ff1f2325c5ac2b6813e378361d3a535
Jeff <33181256+milnak@users.noreply.github.com>
2024-09-10 12:05:25 -0700

    UpdateCheck: use shared method

--- UpdateCheck.ps1
+++ UpdateCheck.ps1
@@ -1,70 +1,55 @@
 ﻿Param([string]$Path = '.')
 
+function Check-GithubUpdate {
+    param (
+        [Parameter(Mandatory)][string]$Path,
+        [Parameter(Mandatory)][string]$Repo
+    )
+
+    $vpxItem = Get-Item -LiteralPath $Path
+
+    $vpxVer = 'v{0}' -f $vpxItem.VersionInfo.ProductVersion
+
+    $json = (Invoke-WebRequest -Uri ('https://api.github.com/repos/{0}/releases' -f $Repo)).Content | ConvertFrom-Json
+
+    $tag_name = @($json.tag_name)[0] -replace '-', '.'
+
+    @{
+        OnlineVersion = $tag_name
+        LocalVersion  = $vpxVer
+        LocalPath     = $Path
+        Assets        = $json.assets.browser_download_url | Where-Object { $_ -match $tag_name }
+    }
+}
+
 ### Visual Pinball X
 
 Write-Host -ForegroundColor Cyan 'Visual Pinball X:'
+$result = Check-GithubUpdate -Path (Join-Path -Path $Path -ChildPath 'VPinballX64.exe') -Repo 'vpinball/vpinball'
+'Local version:  {0} ({1})' -f $result.LocalVersion, $result.LocalPath
+'Online version: {0}' -f $result.OnlineVersion
 
-$vpx = Join-Path -Path $Path -ChildPath 'VPinballX64.exe'
-
-$vpxItem = Get-Item  $vpx -ErrorAction SilentlyContinue
-if ($vpxItem) {
-    $vpxVer = 'v{0}' -f $vpxItem.VersionInfo.ProductVersion
-
-    try {
-        $req = Invoke-WebRequest -Uri 'https://api.github.com/repos/vpinball/vpinball/releases'
-    }
-    catch {
-        Write-Warning "Unable to connect to api.github.com: $_"
-        return
-    }
-
-    $json = $req.Content | ConvertFrom-Json
-    $tag = $json.tag_name[0] -replace '-', '.'
-    "Online version: $tag"
-    "Local version:  $vpxVer ($vpxItem)"
-
-    if ($vpxVer -ne $tag) {
-        Write-Host -ForegroundColor Yellow 'VPX Update available from https://github.com/vpinball/vpinball/releases'
-    }
-    else {
-        Write-Host -ForegroundColor Green 'Latest version installed.'
-    }
+if ($result.LocalVersion -ne $result.OnlineVersion) {
+    Write-Host -ForegroundColor Yellow 'VPX Update available from https://github.com/vpinball/vpinball/releases :'
+    $result.Assets
 }
 else {
-    "Unable to find $vpx"
+    Write-Host -ForegroundColor Green 'Latest version installed.'
 }
+
+''
 
 ### Visual PinMAME
 
 Write-Host -ForegroundColor Cyan 'Visual PinMAME:'
+$result = Check-GithubUpdate -Path (Join-Path -Path $Path -ChildPath 'VPinMAME\VPinMAME64.dll') -Repo 'vpinball/pinmame'
+'Local version:  {0} ({1})' -f $result.LocalVersion, $result.LocalPath
+'Online version: {0}' -f $result.OnlineVersion
 
-$vpm = Join-Path -Path $Path -ChildPath 'VPinMAME\VPinMAME64.dll'
-$vpmItem = Get-Item  $vpm -ErrorAction SilentlyContinue
-if ($vpmItem) {
-    $vpmVer = 'v{0}' -f $vpmItem.VersionInfo.ProductVersion
-
-    try {
-        $req = Invoke-WebRequest -Uri 'https://api.github.com/repos/vpinball/pinmame/releases'
-    }
-    catch {
-        Write-Warning "Unable to connect to api.github.com: $_"
-        return
-    }
-
-    $json = $req.Content | ConvertFrom-Json
-    $tag = $json.tag_name[0] -replace '-', '.'
-    "Online version: $tag"
-    "Local version:  $vpmVer ($vpmItem)"
-
-    if ($vpmVer -ne $tag) {
-        Write-Host -ForegroundColor Yellow 'VPM Update available from https://github.com/vpinball/pinmame/releases'
-    }
-    else {
-        Write-Host -ForegroundColor Green 'Latest version installed.'
-    }
+if ($result.LocalVersion -ne $result.OnlineVersion) {
+    Write-Host -ForegroundColor Yellow 'VPM Update available from https://github.com/vpinball/pinmame/releases :'
+    $result.Assets
 }
 else {
-    "Unable to find $vpm"
+    Write-Host -ForegroundColor Green 'Latest version installed.'
 }
-
-
