15339f161f56602db3a8edd76a724fd205e36769
Jeff <33181256+milnak@users.noreply.github.com>
2025-04-24 15:00:10 -0700

    Update UpdateCheck.ps1

--- UpdateCheck.ps1
+++ UpdateCheck.ps1
@@ -8,17 +8,17 @@ function Check-GithubUpdate {
 
     $vpxItem = Get-Item -LiteralPath $Path -ErrorAction Stop
 
-    $vpxVer = 'v{0}' -f $vpxItem.VersionInfo.ProductVersion
+    $localVersion = 'v{0}' -f $vpxItem.VersionInfo.ProductVersion
 
     $json = (Invoke-WebRequest -Uri ('https://api.github.com/repos/{0}/releases' -f $Repo)).Content | ConvertFrom-Json
 
-    $tag_name = @($json.tag_name)[0] -replace '-', '.'
+    $onlineVersion = @($json.onlineVersion)[0] -replace '-', '.'
 
     @{
-        OnlineVersion = $tag_name
-        LocalVersion  = $vpxVer
-        LocalPath     = $Path
-        Assets        = $json.assets.browser_download_url | Where-Object { $_ -match $tag_name }
+        OnlineVersion = $onlineVersion
+        LocalVersion  = $localVersion
+        Path          = $Path
+        Assets        = $json.assets.browser_download_url | Where-Object { $_ -match $onlineVersion }
     }
 }
 
@@ -26,13 +26,12 @@ function Check-GithubUpdate {
 
 Write-Host -ForegroundColor Cyan 'Visual Pinball X:'
 $result = Check-GithubUpdate -Path (Join-Path -Path $Path -ChildPath 'VPinballX64.exe') -Repo 'vpinball/vpinball'
-'Local version:  {0} ({1})' -f $result.LocalVersion, $result.LocalPath
+'Local version:  {0} ({1})' -f $result.LocalVersion, $result.Path
 'Online version: {0}' -f $result.OnlineVersion
 
 if ($result.LocalVersion -ne $result.OnlineVersion) {
-    Write-Host -ForegroundColor Yellow 'VPX Update available from https://github.com/vpinball/vpinball/releases :'
-    $result.Assets | Where-Object { $_ -like '*/VPinballX-*-windows-x64-Release.zip' }
-    '(Extract to "Visual Pinball" root directory)'
+    Write-Host -ForegroundColor Yellow 'VPX Update available (Extract to "Visual Pinball" root directory)!'
+    $result.Assets | Where-Object { $_ -like '*/VPinballX-*-windows-x64-Release.zip' -and $_ -notlike '*-dev-third-party-*' }
 }
 else {
     Write-Host -ForegroundColor Green 'Latest version installed.'
@@ -44,13 +43,12 @@ else {
 
 Write-Host -ForegroundColor Cyan 'Visual PinMAME:'
 $result = Check-GithubUpdate -Path (Join-Path -Path $Path -ChildPath 'VPinMAME\VPinMAME64.dll') -Repo 'vpinball/pinmame'
-'Local version:  {0} ({1})' -f $result.LocalVersion, $result.LocalPath
+'Local version:  {0} ({1})' -f $result.LocalVersion, $result.Path
 'Online version: {0}' -f $result.OnlineVersion
 
 if ($result.LocalVersion -ne $result.OnlineVersion) {
-    Write-Host -ForegroundColor Yellow 'VPM Update available from https://github.com/vpinball/pinmame/releases :'
+    Write-Host -ForegroundColor Yellow 'VPM Update available (Extract to "VPinMAME" subdirectory)!'
     $result.Assets | Where-Object { $_ -like '*/VPinMAME-sc-*-win-x64.*' }
-    '(Extract to "VPinMAME" subdirectory)'
 }
 else {
     Write-Host -ForegroundColor Green 'Latest version installed.'
