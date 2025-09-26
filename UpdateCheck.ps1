b8421afddc719392d474e686abfcf3a7e33b3e52
Jeff <33181256+milnak@users.noreply.github.com>
2025-09-26 08:38:23 -0700

    UpdateCheck: add dmd-extensions

--- UpdateCheck.ps1
+++ UpdateCheck.ps1
@@ -6,9 +6,13 @@ function Check-GithubUpdate {
         [Parameter(Mandatory)][string]$Repo
     )
 
-    $vpxItem = Get-Item -LiteralPath $Path -ErrorAction Stop
-
-    $localVersion = 'v{0}' -f $vpxItem.VersionInfo.ProductVersion
+    $vpxItem = Get-Item -LiteralPath $Path -ErrorAction SilentlyContinue
+    if ($vpxItem) {
+        $localVersion = 'v{0}' -f $vpxItem.VersionInfo.ProductVersion
+    }
+    else {
+        $localVersion = '0.0.0.0'
+    }
 
     $json = (Invoke-WebRequest -Uri ('https://api.github.com/repos/{0}/releases' -f $Repo)).Content | ConvertFrom-Json
 
@@ -42,15 +46,35 @@ else {
 
 ### Visual PinMAME
 
+mkdir 'VPinMAME' -ErrorAction SilentlyContinue
+mkdir 'VPinMAME/roms' -ErrorAction SilentlyContinue
+
 Write-Host -ForegroundColor Cyan 'Visual PinMAME:'
-$destination = Resolve-Path -LiteralPath (Join-Path -Path $Path -ChildPath 'VPinMAME')
-$result = Check-GithubUpdate -Path (Join-Path -Path $destination -ChildPath 'VPinMAME64.dll') -Repo 'vpinball/pinmame'
+$vpinmame_path = Resolve-Path -LiteralPath (Join-Path -Path $Path -ChildPath 'VPinMAME') -ErrorAction SilentlyContinue
+$destination = Join-Path -Path $vpinmame_path -ChildPath 'VPinMAME64.dll'
+$result = Check-GithubUpdate -Path $destination -Repo 'vpinball/pinmame'
 'Local version:  {0} ({1})' -f $result.LocalVersion, $result.Path
 'Online version: {0}' -f $result.OnlineVersion
 
 if ($result.LocalVersion -ne $result.OnlineVersion) {
-    Write-Host -ForegroundColor Yellow "VPM Update available (Extract to '$destination'):"
+    Write-Host -ForegroundColor Yellow "VPM Update available (Extract to '$vpinmame_path'):"
     $result.Assets | Where-Object { $_ -like '*/VPinMAME-sc-*-win-x64.*' }
+}
+else {
+    Write-Host -ForegroundColor Green 'Latest version installed.'
+}
+
+### dmd-extensions
+
+Write-Host -ForegroundColor Cyan 'dmd-extensions:'
+$destination = Join-Path -Path $vpinmame_path -ChildPath 'dmdext.exe'
+$result = Check-GithubUpdate -Path $destination -Repo 'freezy/dmd-extensions'
+'Local version:  {0} ({1})' -f $result.LocalVersion, $result.Path
+'Online version: {0}' -f $result.OnlineVersion
+
+if ($result.LocalVersion -ne $result.OnlineVersion) {
+    Write-Host -ForegroundColor Yellow "dmd-extensions Update available (Extract to '$vpinmame_path'):"
+    $result.Assets | Where-Object { $_ -like '*/dmdext-v*-x64.zip' }
 }
 else {
     Write-Host -ForegroundColor Green 'Latest version installed.'
