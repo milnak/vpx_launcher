43db9dbe4825110c7707b3e5ac083411c1c941f6
Jeff <33181256+milnak@users.noreply.github.com>
2025-12-05 11:23:51 -0800

    Don't return Markdown

--- Get-TableDetails.ps1
+++ Get-TableDetails.ps1
@@ -1,5 +1,4 @@
-# .\Get-TableDetails.ps1 | Select-Object FileName, TableName, TableVersion, AuthorName `
-# | ConvertTo-Csv -NoTypeInformation | Out-File -LiteralPath 'TableDetails.csv' -Encoding UTF8
+# .\Get-TableDetails.ps1 | Select-Object FileName, TableName, TableVersion, AuthorName | ConvertTo-Csv -NoTypeInformation
 
 [CmdletBinding()]
 Param(
@@ -314,7 +313,7 @@ function Read-VpxMetadata {
     #
 
     $metadata = [PSCustomObject]@{
-        'FileName' = (Split-Path -Leaf $Path)
+        'FileName' = (Get-Item -LiteralPath $Path).BaseName
     }
 
     $StartTime = Get-Date
@@ -401,7 +400,48 @@ function Read-VpxMetadata {
 # MAIN
 #
 
+# '# Table Details'
+# ''
+
+# REVIEW: Is .Version binary?
+# REVIEW: Are .CustomInfoTags binary?
 Get-ChildItem -LiteralPath $TablePath -File -Filter '*.vpx' | ForEach-Object {
     Write-Progress -Activity "Processing Tables" -Status $_.BaseName
     Read-VpxMetadata -Path $_.FullName
+    # $metadata = Read-VpxMetadata -Path $_.FullName
+    # foreach ($value in $metadata) {
+    #     '## {0}' -f $value.Filename
+    #     ''
+    #     'Name: {0}' -f $value.TableName
+    #     ''
+    #     'Version: {0} - {1}' -f $value.TableVersion, $value.ReleaseDate
+    #     ''
+    #     'Author: {0} ({1}) - {2}' -f $value.AuthorName, $value.AuthorEmail, $value.AuthorWebSite
+    #     ''
+    #     ''
+    #     if ($value.TableInfo) {
+    #         '### Info'
+    #         ''
+    #         # '```text'
+    #         $value.TableInfo
+    #         # '```'
+    #         ''
+    #     }
+    #     if ($value.TableDescription) {
+    #         '### Description'
+    #         ''
+    #         # '```text'
+    #         $value.TableDescription
+    #         # '```'
+    #         ''
+    #     }
+    #     if ($value.TableRules) {
+    #         '### Rules'
+    #         ''
+    #         # '```text'
+    #         $value.TableRules
+    #         # '```'
+    #         ''
+    #     }
+    # }
 }
